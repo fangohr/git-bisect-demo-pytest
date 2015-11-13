f963cc4eabf8e41635f3aa005e9aaae675a9d169
Hans Fangohr <fangohr@soton.ac.uk>
2015-11-13 14:22:10 +0000

    extend docstring

--- lib.py
+++ lib.py
@@ -1,5 +1,6 @@
 def f(x, y):
     """An important function. That returns a tuple.
-    Of which the first element is x*y."""
+    Of which the first element is x*y and the second
+    is x+y."""
     prod = x*y
     return prod, x+y
