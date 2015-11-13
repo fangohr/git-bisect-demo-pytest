f597773b9eef0509d7a3b5c56bf78a452a838a65
Hans Fangohr <fangohr@soton.ac.uk>
2015-11-13 14:23:28 +0000

    pep8 improvements

--- lib.py
+++ lib.py
@@ -2,6 +2,6 @@ def f(x, y):
     """An important function. That returns a tuple.
     Of which the first element is x*y and the second
     is x+y."""
-    prod = x*y
-    sum_ = x*y
+    prod = x * y
+    sum_ = x * y
     return prod, sum_
